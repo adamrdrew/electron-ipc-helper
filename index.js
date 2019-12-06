e23f478b664f21df24fdea18d642a1ba3647cb3e
Adam Drew <adamrdrew@live.com>
2019-12-06 07:34:23 -0500

    Removed a useless promise. README for life.

--- index.js
+++ index.js
@@ -1,46 +1,4 @@
-
-class MessageResponder {
-  constructor (requestEvent) {
-    this.requestEvent = requestEvent
-  }
-
-  respond (callback) {
-    return new Promise((resolve, reject) => {
-      ipcMain.on(this.requestEvent, (event, response) => {
-        this.responseEvent = response.responseEvent
-        this.event = event
-        this.send(callback(response.arg))
-      })
-    })
-  }
-
-  send (response) {
-    this.event.reply(this.responseEvent, response)
-  }
-}
-
-class MessageSender {
-  constructor (requestEvent) {
-    this.requestEvent = requestEvent
-  }
-
-  send (arg = false) {
-    return new Promise((resolve, reject) => {
-      try {
-        const responseEvent = this.newResponseEvent()
-        ipcRenderer.once(responseEvent, (event, response) => {
-          resolve(response)
-        })
-        ipcRenderer.send(this.requestEvent, { responseEvent, arg })
-      } catch (err) {
-        reject(err)
-      }
-    })
-  }
-
-  newResponseEvent () {
-    return `${this.requestEvent}-response-${uuid()}`
-  }
-}
+import MessageResponder from './lib/MessageResponder'
+import MessageSender from './lib/MessageSender'
 
 export { MessageResponder, MessageSender }
